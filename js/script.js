e4109dbef818810cfec43a53b3c8535104082cdb
Marcin Miras <marcinmiras@gmail.com>
2021-01-04 19:53:31 +0000

    add JS article section files

--- js/script.js
+++ js/script.js
@@ -26,29 +26,42 @@ function titleClickHandler(event){
   }
   /*[IN PROGRESS] add class 'active' to the clicked link */
   console.log('clickedElement:', clickedElement);
-
-    clickedElement.classList.add('active');
-
-// !!!!!!!!!!!!!!Dlaczego to działa, przecież to nawet nie jest funkcja?? :O !!!!!!!!!!
-
-
+  clickedElement.classList.add('active');
 
   /* [DONE] remove class 'active' from all articles */
  const activeArticles = document.querySelectorAll('.posts .active')
 
  for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
-
  }
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector)
+  console.log(targetArticle);
+  console.trace(targetArticle)
+  
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
+  console.log(targetArticle)
+
+  const links = document.querySelectorAll('.titles a');
+
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
 }
+
+// dlaczego to zostało wrzucone poza nawias? W ćwiczeniu nie działa bo jest poza nawiasem.
 
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+// dlaczego to zostało wrzucone poza nawias? W ćwiczeniu nie działa bo jest poza nawiasem.
